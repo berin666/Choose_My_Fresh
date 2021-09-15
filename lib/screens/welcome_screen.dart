16dfb779c85194870bbfdfba0a4b7bff3077920f
mubasheeraCB1517 <mubasheeraglobosoft@gmail.com>
2021-09-15 13:26:30 +0530

    second commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:grocery_app/common_widgets/app_button.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
+import 'package:grocery_app/screens/dashboard/dashboard_screen.dart';
+import 'package:grocery_app/styles/colors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  final String imagePath = "assets/images/welcome_image.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: AppColors.primaryColor,
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Spacer(),
+                icon(),
+                SizedBox(
+                  height: 20,
+                ),
+                welcomeTextWidget(),
+                SizedBox(
+                  height: 10,
+                ),
+                sloganText(),
+                SizedBox(
+                  height: 40,
+                ),
+                getButton(context),
+                SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  Widget icon() {
+    String iconPath = "assets/icons/choose-icon.png";
+    return Image.asset(
+      iconPath,
+      width: 48,
+      height: 56,
+    );
+  }
+
+  Widget welcomeTextWidget() {
+    return Column(
+      children: [
+        AppText(
+          text: "Welcome",
+          fontSize: 35,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+        ),
+        AppText(
+            text: "to",
+            fontSize: 35,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+            textAlign: TextAlign.center
+        ),
+        Container(
+        margin: EdgeInsets.only(left: 5,right: 5),
+        child:AppText(
+          text: "Choose My Fresh",
+          fontSize: 35,
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+          textAlign: TextAlign.center
+        ),
+    )
+      ],
+    );
+  }
+
+  Widget sloganText() {
+    return AppText(
+      text: "Get your grecories as fast as in hour",
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: Color(0xffFCFCFC).withOpacity(0.7),
+    );
+  }
+
+  Widget getButton(BuildContext context) {
+    return AppButton(
+      label: "Get Started",
+      fontWeight: FontWeight.w600,
+      padding: EdgeInsets.symmetric(vertical: 25),
+      onPressed: () {
+        onGetStartedClicked(context);
+      },
+    );
+  }
+
+  void onGetStartedClicked(BuildContext context) {
+    Navigator.of(context).pushReplacement(new MaterialPageRoute(
+      builder: (BuildContext context) {
+        return DashboardScreen();
+      },
+    ));
+  }
+}
---
+++ lib/styles/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/styles/colors.dart';
+
+String gilroyFontFamily = "Poppins";
+
+ThemeData themeData = ThemeData(
+  accentColor: AppColors.primaryColor,
+  fontFamily: gilroyFontFamily,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
